f9f50bd2a356ecaf69fead13386a41123fda6ddb
maldrek3103 <cpisanu96@gmail.com>
2021-07-22 16:19:20 +0200

    Made card template and print on page

--- js/script.js
+++ js/script.js
@@ -36,6 +36,15 @@ const card = {
         //Total cards in the edition
         totalCard: 350
     },
+    //Card ability/abilities
+    abilities: [
+        {
+            //Mana cost of the ability & how to use it
+            manaCost: ['R', 'T'],
+            //Ability effects
+            description: 'Sacrifice Bloodfire Colossus: Bloodfire Colossus deals 6 damage to each creature and each player.'
+        }
+    ],
     //Emphasised text section (descriptive, not useful)
     flavorText: {
         //Text content
@@ -43,15 +52,6 @@ const card = {
         //Text author
         author: 'None'
     },
-    //Card ability/abilities
-    abilities: [
-        {
-            //Mana cost of the ability & how to use it
-            launchCost: ['R', 'T'],
-            //Ability effects
-            description: 'Sacrifice Bloodfire Colossus: Bloodfire Colossus deals 6 damage to each creature and each player.'
-        }
-    ],
     //Card strength
     strength: 5,
     //Card constitution
@@ -65,3 +65,67 @@ const card = {
         pattern: '..img/background_pattern.jpg'
     }
 };
+
+
+// Print on page
+const cardSection = document.getElementById('cards');
+
+//Condition that verifies if there is a card sub type
+const subType = card.subType ? ` - ${card.subType}` : '';
+
+//Ability default (not all the cards have abilities)
+let abilitiesContent = "<em>No abilities</em>";
+
+//Condition that verifies if there is/are ability/abilities
+if (card.abilities.length) {
+    abilitiesContent = '<ul>';
+    for (let i = 0; i < card.abilities.length; i++) {
+        const currentAbility = card.abilities[i];
+        abilitiesContent += `<li><strong>Mana cost:</strong> ${currentAbility.manaCost.join(', ')}</li>`;
+        abilitiesContent += `<li><strong>Description:</strong> ${currentAbility.description}</li>`;
+    }
+    abilitiesContent += '</ul>';
+}
+
+//General card structure 
+let cardTemplate = `
+<ul class="card"> 
+    <li><strong>Card ID:</strong> ${card.id}</li>
+    <li><strong>Name:</strong> ${card.name}</li>
+    <li><strong>Mana cost:</strong> ${card.manaCost.join(', ')}</li>
+    <li><strong>CMC (Converted Mana Cost):</strong> ${card.convertedManaCost}</li>
+    <li><strong>Illustration:</strong> 
+        <ul>
+            <li><strong>Author:</strong> ${card.illustration.author.fullName} (ID: ${card.illustration.author.id})</li>
+            <li><strong>Illustration link:</strong> ${card.illustration.source}</li>
+        </ul>
+    </li>
+    <li><strong>Card type:</strong> ${card.type} ${subType}</li >
+    <li><strong>Expansion:</strong> 
+        <ul>
+            <li><strong>Reprint:</strong> ${card.expansion.reprintId}</li>
+            <li><strong>Name:</strong> ${card.expansion.name}</li>
+            <li><strong>Rarity:</strong> ${card.expansion.rarity}</li>
+            <li><strong>Collection number:</strong> ${card.expansion.collectionNr}/${card.expansion.totalCard}</li>
+        </ul>
+    </li>
+    <li><strong>Ability/Abilities:</strong> ${abilitiesContent}</li>
+    <li><strong>Flavor text:</strong>
+        <ul>
+        <li><strong>Quote:</strong> <em>${card.flavorText.quote}</em></li>
+        <li><strong>Author:</strong> <em>${card.flavorText.author}</em></li>
+        </ul>
+    </li>
+    <li><strong>Constitution:</strong> ${card.constitution}</li>
+    <li><strong>Strength:</strong> ${card.strength}</li>
+    <li><strong>Border color:</strong> ${card.borderColor}</li>
+    <li><strong>Background:</strong>
+        <ul>
+            <li><strong>Color:</strong> ${card.background.color}</li>
+            <li><strong>Pattern:</strong> ${card.background.pattern}</li>
+        </ul>
+    </li>
+ </ul > `;
+
+//Prints card template on page
+cardSection.innerHTML = cardTemplate;
